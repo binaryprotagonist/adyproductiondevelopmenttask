6272752097fa94ebc37ad43c1f6a220ae281bf9b
Prateek <kaushikprateek11@gmail.com>
2020-04-11 12:14:42 +0530

    Updates

---
+++ precache-manifest.fa20b2125b50d14561313e9020c2d58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e58e9b5cd8b8acf98ad0768a1982f19",
+    "url": "/adyproductiondevelopmenttask/index.html"
+  },
+  {
+    "revision": "55f226598c2ee513c1a9",
+    "url": "/adyproductiondevelopmenttask/static/css/main.e8a7ceb8.chunk.css"
+  },
+  {
+    "revision": "78fcfeab190bd771e8d5",
+    "url": "/adyproductiondevelopmenttask/static/js/2.e7520557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adyproductiondevelopmenttask/static/js/2.e7520557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f226598c2ee513c1a9",
+    "url": "/adyproductiondevelopmenttask/static/js/main.fcff3971.chunk.js"
+  },
+  {
+    "revision": "bde28c6e38ad8d77dd19",
+    "url": "/adyproductiondevelopmenttask/static/js/runtime-main.98c1a7bb.js"
+  }
+]);
